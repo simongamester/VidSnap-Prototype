da70460d2471d98e736c06428294a6e29dec031e
Simon Gamester <simon@rootsol.co.uk>
2018-10-19 18:41:44 +0100

    Upload working with tus

--- prototype3/server.php
+++ prototype3/server.php
@@ -1,6 +1,8 @@
 <?php
 include 'vendor/autoload.php';
 
+\TusPhp\Config::set('config.dev.php');
+
 $server   = new \TusPhp\Tus\Server(); // Leave empty for file based cache
 $response = $server->serve();
 
